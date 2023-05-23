d4a83c0aa810d1e9d4d36e39693355107819dba9
fer-gif <ferminhernando00@gmail.com>
2023-05-22 21:37:00 -0300

    cambiosFer

---
+++ model/TorneoModel.php
@@ -0,0 +1,52 @@
+<?php
+    class TorneoModel{
+        
+       
+        private $db;
+
+        public function __construct(){
+            $this->db=new PDO("mysql");
+          
+
+        }
+
+        
+
+        public function getTorneo(){
+            $sentence = $this->db->prepare( "select Nombre, Puntos AS ,Partidos-Jugados AS PJ, Ganados AS PG, Perdidos AS PP, Empatados AS PE, Goles  from equipos ORDER BY Puntos ASC");
+            $sentence->execute();
+            $positions = $sentence->fetchAll(PDO::FETCH_OBJ);
+            return $positions;
+
+        }
+
+        public function deleteEquipo($id){
+            $sentence = $this->db->prepare("DELETE FROM equipos WHERE ID_Equipo=?");
+            $response = $sentence->execute(array($id));
+        }
+
+        public function addEquipo($nombre){
+            $sentence = $this->db->prepare("INSERT INTO equipos(Nombre) VALUES(?)");
+            $sentence->execute(array($nombre));
+            return $this->db->lastInsertId();
+    
+        }
+
+        public function deleteJugador($id){
+            $sentence = $this->db->prepare("DELETE FROM jugadores WHERE ID_Jquipo=?");
+            $response = $sentence->execute(array($id));
+        }
+        
+        public function addJugador($nombre,$apellido,$dni,$pos,$tel,$equipo){
+            $sentence = $this->db->prepare("INSERT INTO equipos(Nombre,Apellido,DNI,Posicion,Numero_Tel,ID_Equipo) VALUES(?,?,?,?,?,?)");
+            $sentence->execute(array($nombre,$apellido,$dni,$pos,$tel,$equipo));
+            return $this->db->lastInsertId();
+        }
+        
+
+}
+
+
+
+
+?>
