22eeed59f8da33243e2a48a06790d631a1834ff7
fer-gif <ferminhernando00@gmail.com>
2023-06-01 16:38:46 -0300

    agrego funciones a partido

--- model/JugadorModel.php
+++ model/JugadorModel.php
@@ -1,12 +1,31 @@
 <?php
+require_once 'model/Conexion.php';
 
 class JugadorModel
 {
+    private $connection;
+    private $id_jugador;
+    private $nombre;
+    private $apellido;
+    private $dni;
+    private $numero_tel;
+    private $posicion;
+    private $equipo;
+
+    public function __construct($id_jugador,$nombre,$apellido,$dni,$numero_tel,$posicion,$equipo)
+    {
+        $this->id_jugador=$id_jugador;
+        $this->nombre=$nombre;
+        $this->apellido=$apellido;
+        $this->dni=$dni;
+        $this->numero_tel=$numero_tel;
+        $this->posicion=$posicion;
+        $this->equipo=$equipo;
+        $this->connection=new Conexion();
+    }
 
 
-
-
-    public function getJugadores($idEquipo)
+    public function getJugadoresxEquipo($idEquipo)
     {
         $conexion=$this->connection->getConnection();
         $sentence = $conexion->prepare("SELECT * FROM jugadores j 
@@ -20,4 +39,59 @@ class JugadorModel
         
         return $jugadores;
     }
+
+    public function getJugador($id)
+    {
+        $conexion=$this->connection->getConnection();
+        $sentence = $conexion->prepare("SELECT * FROM jugadores 
+                                        WHERE id_jugador=?");
+        $sentence->execute(array($id));
+
+        $sentence->setFetchMode(PDO::FETCH_ASSOC);
+        $jugador = $sentence->fetchAll();
+        
+        return $jugador;
+    }
+
+    public function addJugador($nombre,$apellido,$dni,$telefono,$posicion,$id_equipo,)
+    {
+        $conexion = $this->connection->getConnection();
+        $sentence = $conexion->prepare("INSERT INTO jugadores j(j.nombre,j.apellido,j.dni,j.telefono,j.posicion,j.id_equipo) 
+                                        INNER JOIN equipos e
+                                        ON e.id_equipo=j.id_equipo
+                                        VALUES(?,?,?,?,?,?)");
+        $sentence->execute(array($nombre,$apellido,$dni,$telefono,$posicion,$id_equipo,));
+        $lastId = $conexion->lastInsertId();
+        $conexion = null;
+
+        return $lastId;
+
+    }
+
+    public function deleteJugador($id_jugador)
+    {
+        $conexion = $this->connection->getConnection();
+        $sentence = $conexion->prepare("DELETE FROM jugadores WHERE id_jugador=?");
+        $response = $sentence->execute(array($id_jugador));
+        $conexion = null;
+
+        return $response;
+    }
+
+    public function updateJugador($nombre,$apellido,$dni,$telefono,$posicion,$id_equipo)
+    {
+        $conexion = $this->connection->getConnection();
+        $sentence = $conexion->prepare("UPDATE jugadores
+                                    SET nombre = ? OR apellido=? OR dni=? OR telefono=? OR posicion=? OR id_equipo=?
+                                    WHERE id_jugador = ?");
+        $response = $sentence->execute(array($nombre,$apellido,$dni,$telefono,$posicion,$id_equipo));
+        $conexion = null;
+
+        return $response;
+
+    }
+
+    
+
+
 }
--- model/PartidoModel.php
+++ model/PartidoModel.php
@@ -1,5 +1,48 @@
 <?php
-
+require_once 'model/Conexion.php';
 class PartidoModel
 {
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection=new Conexion();
+    }
+
+    public function getPartidos()
+    {
+
+    }
+
+    public function getPartidoxEquipo()
+    {}
+
+    public function addPartido()
+    {}
+
+    public function deletePartido()
+    {}
+
+    public function updatePartido()
+    {}
+
+    public function getResultado()
+    {}
+
+    public function getGolesDeEqipo()
+    {}
+
+    public function getGolesRecibidos()
+    {}
+
+    public function getPartidoPerdido()   
+    {}
+
+    public function getPartidoGanado()
+    {}
+
+    public function getPartidoEmpatado()
+    {}
+    
+
 }
