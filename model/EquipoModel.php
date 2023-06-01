38015787ea12f620c7f64230ef50f8613c9f57fc
Ferher00 <ferminhernando00@gmail.com>
2023-05-31 23:31:31 -0300

    funciones del model equipo y algo de jugadores

--- model/EquipoModel.php
+++ model/EquipoModel.php
@@ -1,5 +1,70 @@
 <?php
-
+require_once 'model/Conexion.php';
 class EquipoModel
 {
+    private $nombre;
+    private $id_equipo;
+    private $connection;
+
+    public function __construct($id_equipo,$nombre)
+    {
+        $this->connection=new Conexion();
+        $this->nombre=$nombre;
+        $this->id_equipo=$id_equipo;        
+    }
+
+    public function getEquipos()
+    {
+        $conexion=$this->connection->getConnection();
+        $sentence = $conexion->prepare("SELECT * FROM equipos");
+        $sentence->execute();
+        $sentence->setFetchMode(PDO::FETCH_ASSOC);
+        $equipos = $sentence->fetchAll();
+        $conexion = null;
+        return $equipos;
+    }
+    
+
+    public function addEquipo($nombre)
+    {
+        $conexion = $this->connection->getConnection();
+        $sentence = $conexion->prepare("INSERT INTO equipos(nombre) VALUES(?)");
+        $sentence->execute(array($nombre));
+        $lastId = $conexion->lastInsertId();
+        $conexion = null;
+        return $lastId;
+    }
+
+    public function deleteEquipo($idEquipo)
+    {
+        $conexion = $this->connection->getConnection();
+        $sentence = $conexion->prepare("DELETE FROM equipos WHERE id_equipo=?");
+        $response = $sentence->execute(array($idEquipo));
+        $conexion = null;
+    }
+
+    public function updateEquipo($idEquipo)
+    {
+
+    }
+
+    public function getEquipo($idEquipo)
+    {
+        $conexion=$this->connection->getConnection();
+        $sentence = $conexion->prepare("SELECT * FROM equipos
+                                        WHERE id_equipo=?");
+        $sentence->execute(array($idEquipo));
+
+        $sentence->setFetchMode(PDO::FETCH_ASSOC);
+        $equipo = $sentence->fetchAll();
+        
+        return $equipo;
+    }
+    
+
+
+
+
+    public function 
+
 }
--- model/JugadorModel.php
+++ model/JugadorModel.php
@@ -2,4 +2,22 @@
 
 class JugadorModel
 {
+
+
+
+
+    public function getJugadores($idEquipo)
+    {
+        $conexion=$this->connection->getConnection();
+        $sentence = $conexion->prepare("SELECT * FROM jugadores j 
+                                        INNER JOIN equipos e
+                                        ON j.id_equipo=e.id_equipo
+                                        WHERE e.id_equipo=?");
+        $sentence->execute(array($idEquipo));
+
+        $sentence->setFetchMode(PDO::FETCH_ASSOC);
+        $jugadores = $sentence->fetchAll();
+        
+        return $jugadores;
+    }
 }
